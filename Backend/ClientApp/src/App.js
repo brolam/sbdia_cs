0b9abf24cd3e1743638099c8917d462d977d38da
brolam <breno@brolam.com.br>
2020-10-18 14:51:24 +0000

    Changed App Component to Function

--- Backend/ClientApp/src/App.js
+++ Backend/ClientApp/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
@@ -11,18 +11,14 @@ import { ApplicationPaths } from './components/api-authorization/ApiAuthorizatio
 
 import './custom.css'
 
-export default class App extends Component {
-  static displayName = App.name;
-
-  render() {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <AuthorizeRoute path='/fetch-data' component={FetchData} />
-        <AuthorizeRoute path='/sensors' component={SensorsHome} />
-        <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
-      </Layout>
-    );
-  }
+export default function App(props) {
+  return (
+    <Layout>
+      <Route exact path='/' component={Home} />
+      <Route path='/counter' component={Counter} />
+      <AuthorizeRoute path='/fetch-data' component={FetchData} />
+      <AuthorizeRoute path='/sensors' component={SensorsHome} />
+      <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
+    </Layout>
+  );
 }
