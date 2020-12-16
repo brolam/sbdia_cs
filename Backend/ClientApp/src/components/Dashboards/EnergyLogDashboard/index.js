d4c15bffd9eab7c56115c89a1b4713eac833eb1d
brolam <breno@brolam.com.br>
2020-12-16 03:10:16 +0000

    Draft Dashboard logs recent

--- Backend/ClientApp/src/components/Dashboards/EnergyLogDashboard/index.js
+++ Backend/ClientApp/src/components/Dashboards/EnergyLogDashboard/index.js
@@ -2,21 +2,44 @@ import React, { useState, useEffect } from 'react'
 import Chart from "chart.js";
 
 export default function EnergyLogDashboard(props) {
-  const [state, setState] = useState({ sensors: [], loading: true, selectedSensor: null });
+  const date = new Date();
+  const [year, month, day] = [date.getFullYear(), date.getMonth() + 1, date.getDate()]
+  const [state, setState] = useState({
+    sensors: [],
+    loading: true,
+    selectedSensor: null,
+    data: [],
+    seconds: 0
+  });
   var canvaChartRef = React.createRef();
 
   async function populateSensorsList() {
     const response = await fetch('api/sensor', {
       headers: !props.token ? {} : { 'Authorization': `Bearer ${props.token}` }
     });
-    const data = await response.json();
-    setState({ ...state, sensors: data, loading: false });
+    const sensors = await response.json();
+    setState({ ...state, sensors: sensors, loading: false });
+    console.log(state.sensors, props.token);
   }
+
+  async function populateDashboardData() {
+    if (!state.selectedSensor) return;
+    const sensorId = state.selectedSensor.id;
+    const response = await fetch(`api/sensor/${sensorId}/dashboard/${year}/${month}/${day}`, {
+      headers: !props.token ? {} : { 'Authorization': `Bearer ${props.token}` }
+    });
+    const data = await response.json();
+    setState({ ...state, data: data });
+    console.log(state.data, props.token);
+  }
+
+  //setTimeout(() => setState({ ...state, seconds: state.seconds + 1 }), 15000);
 
   useEffect(() => { populateSensorsList(); }, [props.token]);
 
+  useEffect(() => { populateDashboardData(); }, [props.token, state.selectedSensor, state.seconds]);
+
   useEffect(() => {
-    console.log('ctx', canvaChartRef.current)
     new Chart(canvaChartRef.current, {
       type: 'line',
       data: {
@@ -59,7 +82,7 @@ export default function EnergyLogDashboard(props) {
         }
       }
     })
-  })
+  }, [state.data])
 
   const onSelectedSensor = (sensor) => {
     setState({ ...state, selectedSensor: sensor });
@@ -99,131 +122,30 @@ export default function EnergyLogDashboard(props) {
         </div>
       </div>
       <canvas ref={canvaChartRef} className="my-4 w-100" id="myChart" width="1200" height="380"></canvas>
-      <h2>Section title</h2>
+      <h2>Logs</h2>
       <div className="table-responsive">
         <table className="table table-striped table-sm">
           <thead>
             <tr>
-              <th>#</th>
-              <th>Header</th>
-              <th>Header</th>
-              <th>Header</th>
-              <th>Header</th>
+              <th>When</th>
+              <th>Duration</th>
+              <th>Phase 01</th>
+              <th>Phase 02</th>
+              <th>Phase 03</th>
+              <th>Total Watts</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1,001</td>
-              <td>Lorem</td>
-              <td>ipsum</td>
-              <td>dolor</td>
-              <td>sit</td>
-            </tr>
-            <tr>
-              <td>1,002</td>
-              <td>amet</td>
-              <td>consectetur</td>
-              <td>adipiscing</td>
-              <td>elit</td>
-            </tr>
-            <tr>
-              <td>1,003</td>
-              <td>Integer</td>
-              <td>nec</td>
-              <td>odio</td>
-              <td>Praesent</td>
-            </tr>
-            <tr>
-              <td>1,003</td>
-              <td>libero</td>
-              <td>Sed</td>
-              <td>cursus</td>
-              <td>ante</td>
-            </tr>
-            <tr>
-              <td>1,004</td>
-              <td>dapibus</td>
-              <td>diam</td>
-              <td>Sed</td>
-              <td>nisi</td>
-            </tr>
-            <tr>
-              <td>1,005</td>
-              <td>Nulla</td>
-              <td>quis</td>
-              <td>sem</td>
-              <td>at</td>
-            </tr>
-            <tr>
-              <td>1,006</td>
-              <td>nibh</td>
-              <td>elementum</td>
-              <td>imperdiet</td>
-              <td>Duis</td>
-            </tr>
-            <tr>
-              <td>1,007</td>
-              <td>sagittis</td>
-              <td>ipsum</td>
-              <td>Praesent</td>
-              <td>mauris</td>
-            </tr>
-            <tr>
-              <td>1,008</td>
-              <td>Fusce</td>
-              <td>nec</td>
-              <td>tellus</td>
-              <td>sed</td>
-            </tr>
-            <tr>
-              <td>1,009</td>
-              <td>augue</td>
-              <td>semper</td>
-              <td>porta</td>
-              <td>Mauris</td>
-            </tr>
-            <tr>
-              <td>1,010</td>
-              <td>massa</td>
-              <td>Vestibulum</td>
-              <td>lacinia</td>
-              <td>arcu</td>
-            </tr>
-            <tr>
-              <td>1,011</td>
-              <td>eget</td>
-              <td>nulla</td>
-              <td>Class</td>
-              <td>aptent</td>
-            </tr>
-            <tr>
-              <td>1,012</td>
-              <td>taciti</td>
-              <td>sociosqu</td>
-              <td>ad</td>
-              <td>litora</td>
-            </tr>
-            <tr>
-              <td>1,013</td>
-              <td>torquent</td>
-              <td>per</td>
-              <td>conubia</td>
-              <td>nostra</td>
-            </tr>
-            <tr>
-              <td>1,014</td>
-              <td>per</td>
-              <td>inceptos</td>
-              <td>himenaeos</td>
-              <td>Curabitur</td>
-            </tr>
-            <tr>
-              <td>1,015</td>
-              <td>sodales</td>
-              <td>ligula</td>
-              <td>in</td>
-              <td>libero</td>
-            </tr>
+            {state.data.logsRecent && state.data.logsRecent.map((log) => (
+              <tr>
+                <td>{Date(log.unixDate).substring(0, 21)}</td>
+                <td>{log.duration}</td>
+                <td>{log.watts1}</td>
+                <td>{log.watts2}</td>
+                <td>{log.watts3}</td>
+                <td>{log.wattsTotal}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
