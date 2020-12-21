c506eb999e82b0db677fe543b9748ec6df4dea95
brolam <breno@brolam.com.br>
2020-12-21 10:27:21 +0000

    Added draft Sensor Energy Logs Firmware

---
+++ Firmware/EnergyUseLoggerV2/OhaEsp8266/Config_model.h
@@ -0,0 +1,12 @@
+/*
+  Esse código e parte do projeto: https://github.com/brolam/OpenHomeAnalysis
+  @author Breno Marques(https://github.com/brolam) em 12/12/2015.
+  @version 1.00
+*/
+const char* ESP8266_NAME = "OHA_EUL_1"; 
+const char* ESP8266_PASSWORD = "123";
+const char* WIFI_SSID = "[WIFI_SSID]"; 
+const char* WIFI_PASSWORD = "[WIFI_PASSWORD]";
+const char* END_POINT_URL = "http://[IP]:8000/api/sensorLogBatch/"; 
+const char* SENSOR_ID = "[SENSOR_ID]";
+const char* SECRET_API_TOKEN = "[SECRET_API_TOKEN]";
---
+++ Firmware/EnergyUseLoggerV2/OhaEsp8266/Config_prod.h
@@ -0,0 +1,12 @@
+/*
+  Esse código e parte do projeto: https://github.com/brolam/OpenHomeAnalysis
+  @author Breno Marques(https://github.com/brolam) em 12/12/2015.
+  @version 1.00
+*/
+const char* ESP8266_NAME = "OHA_EUL_1"; 
+const char* ESP8266_PASSWORD = "123";
+const char* WIFI_SSID = "BRENO"; 
+const char* WIFI_PASSWORD = "23051800"; //Informar a senha do ESP32.
+const char* END_POINT_URL = "http://35.202.117.127/api/sensorLogBatch/"; 
+const char* SENSOR_ID = "36810735-079c-4d82-8833-48a17565c710";
+const char* SECRET_API_TOKEN = "ac2d5502-7ccb-47bd-b4c0-c2a90c48b713";
