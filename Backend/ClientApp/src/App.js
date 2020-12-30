ea7f2fb70ba115e4c10f7ecacedcad432648fe78
brolam <breno@brolam.com.br>
2020-12-30 21:00:39 +0000

    Removed samples code

--- Backend/ClientApp/src/App.js
+++ Backend/ClientApp/src/App.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
 import SensorsHome from './components/Sensors/SensorsHome';
-import { Counter } from './components/Counter';
 import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
 import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
 import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
@@ -18,8 +16,6 @@ export default function App(props) {
     <Layout>
       <Route exact path='/' component={Home} />
       <AuthorizeRoute path='/dashboards' component={Dashboards} />
-      <Route path='/counter' component={Counter} />
-      <AuthorizeRoute path='/fetch-data' component={FetchData} />
       <AuthorizeRoute exact path='/sensors' component={SensorsHome} />
       <AuthorizeRoute exact path='/sensors/new' component={SensorNew} />
       <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
--- Backend/ClientApp/src/components/NavMenu.js
+++ Backend/ClientApp/src/components/NavMenu.js
@@ -36,13 +36,7 @@ export class NavMenu extends Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <Link className="nav-link" to="/dashboards">Dashboards<span className="sr-only">(current)</span></Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/counter">Counter</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/fetch-data">Fetch data</Link>
+                <Link className="nav-link" to="/dashboards">Dashboard<span className="sr-only">(current)</span></Link>
               </li>
               <SensorMenu isAuthenticated={this.state.isAuthenticated} />
               <LoginMenu />
